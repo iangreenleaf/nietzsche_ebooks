e3bc71a587fa0ab6be637ff7df6a9269d92dcfd7
Ian Young <ian@iangreenleaf.com>
2015-05-22 09:43:13 -0700

    Adding more sources from Project Gutenberg

--- config/sources.rb
+++ config/sources.rb
@@ -5,4 +5,52 @@ SOURCES = [
     headers: [/^PREFACE$/, /^CHAPTER I. PREJUDICES OF PHILOSOPHERS$/],
     footers: [/^End of Project Gutenberg's Beyond Good and Evil/],
   },
+  {
+    title: "Thus Spake Zarathustra",
+    url: "https://www.gutenberg.org/cache/epub/1998/pg1998.txt",
+    headers: [/^THUS SPAKE ZARATHUSTRA.$/, /^FIRST PART. ZARATHUSTRA'S DISCOURSES.$/],
+    footers: [/^APPENDIX.$/],
+  },
+  {
+    title: "The Antichrist",
+    url: "https://www.gutenberg.org/cache/epub/19322/pg19322.txt",
+    headers: [/^PREFACE$/, /^\s+FRIEDRICH W. NIETZSCHE.$/, /^THE ANTICHRIST$/],
+    footers: [/^THE END$/],
+  },
+  {
+    title: "Human, All Too Human",
+    url: "https://www.gutenberg.org/cache/epub/38145/pg38145.txt",
+    headers: [/^PREFACE.$/, /^OF THE FIRST AND LAST THINGS.$/],
+    footers: [/^End of Project Gutenberg's Human, All Too Human/],
+  },
+  {
+    title: "Human, All-Too-Human, Part II",
+    url: "https://www.gutenberg.org/files/37841/37841-0.txt",
+    headers: [/^PART I. MISCELLANEOUS MAXIMS AND OPINIONS.$/],
+    footers: [/^\s+‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐/, /^FOOTNOTES$/],
+  },
+  {
+    title: "The Case Of Wagner",
+    url: "https://www.gutenberg.org/files/25012/25012-0.txt",
+    headers: [/^THE CASE OF WAGNER: A MUSICIAN’S PROBLEM$/, /^Preface$/],
+    footers: [/^FOOTNOTES$/],
+  },
+  {
+    title: "The Case Of Wagner",
+    url: "https://www.gutenberg.org/files/25012/25012-0.txt",
+    headers: [/^THE CASE OF WAGNER: A MUSICIAN’S PROBLEM$/, /^Preface$/],
+    footers: [/^FOOTNOTES$/],
+  },
+  {
+    title: "The Dawn of Day",
+    url: "https://www.gutenberg.org/files/39955/39955-0.txt",
+    headers: [/^AUTHOR’S PREFACE.$/, /^BOOK I.$/],
+    footers: [/^FOOTNOTES$/],
+  },
+  {
+    title: "We Philologists",
+    url: "https://www.gutenberg.org/cache/epub/18267/pg18267.txt",
+    headers: [/^LONDON, _July 1911_.$/],
+    footers: [/^FOOTNOTES:$/],
+  },
 ]
